5aae9f1e91392aff8802aed60e4e9c080921d68d
Samir Raja <samirttt888@gmail.com>
2026-01-12 16:12:46 +0530

    simple_calculator.c

--- simple_calculator.c
+++ simple_calculator.c
@@ -1,29 +1,42 @@
+/*
+ * Module: CLI Arithmetic Engine
+ * Author: Samir Raja
+ * Description: Performs basic mathematical operations with precision handling.
+ */
+
 #include <stdio.h>
-int main(){
+
+int main() {
     char operator;
     double n1, n2;
-    printf("enter an operator (+,-,*,/):");
-    scanf("%c", &operator);
 
-    printf("enter two numbers:");
-    scanf("%lf,%lf", &n1, &n2);
-    switch (operator){
+    printf("\n--- [ C-CALC V1.0 ] ---\n");
+    printf("[>] Enter Operator (+, -, *, /): ");
+    scanf(" %c", &operator); // Space added to consume buffer
+
+    printf("[>] Enter Two Operands (e.g., 10 20): ");
+    scanf("%lf %lf", &n1, &n2);
+
+    printf("\n[RESULT]: ");
+    switch (operator) {
         case '+':
-        printf("result: %.2lf", n1 + n2);
-        break;
+            printf("%.2lf + %.2lf = %.2lf", n1, n2, n1 + n2);
+            break;
         case '-':
-        printf("result: %2lf", n1 - n2);
-        break;
+            printf("%.2lf - %.2lf = %.2lf", n1, n2, n1 - n2);
+            break;
         case '*':
-        printf("result: %2lf", n1 * n2);
-        break;
+            printf("%.2lf * %.2lf = %.2lf", n1, n2, n1 * n2);
+            break;
         case '/':
-        printf("result: %2lf", n1 / n2);
-        break;
+            if (n2 != 0)
+                printf("%.2lf / %.2lf = %.2lf", n1, n2, n1 / n2);
+            else
+                printf("Error! Division by Zero is not allowed.");
+            break;
         default:
-        printf("error! operator is not correct");
-        
+            printf("Error! Invalid Operator Selected.");
     }
+    printf("\n-----------------------\n");
     return 0;
-
 }
